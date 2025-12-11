32e0522604a32234210a6e1e8d3a5b5e0fb5a532
yoshida <shun667_0305@yahoo.co.jp>
2025-12-11 17:28:33 +0900

    不要なコメント削除

--- src/jp/example/app/Main.java
+++ src/jp/example/app/Main.java
@@ -1,7 +1,6 @@
 package jp.example.app;
 
-import jp.example.common.CommonUtil;   // ← 別 src の共通関数を利用
-
+import jp.example.common.CommonUtil;
 public class Main {
 
     public static void main(String[] args) {
