dab6f5ad7d7318f09d3ec14c186df6b6c5c1e8ab
yoshida <shun667_0305@yahoo.co.jp>
2025-12-17 10:26:28 +0900

    メイン修正

--- src/jp/example/app/Main.java
+++ src/jp/example/app/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("=== 実行開始 ===");
-
+    	//
         String path = CommonUtil.createIfNotExists("x.java");
         System.out.println("ファイル確認: " + path);
 
